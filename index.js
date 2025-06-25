738ae0004c58a603227c5955d8bb49aad34c1ebb
Dunamis-001 <alex.nyamai@student.moringaschool.com>
2025-06-25 10:31:30 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution in this file!
+
+let employee = {
+  name: 'Jane Smith',
+  streetAddress: '456 Oak Ave'
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  
+  return { ...employee,
+    [key]: value
+  };
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+  employee[key] = value;
+  return employee;
+}
+
+
+function deleteFromEmployeeByKey(employee, key) {
+ 
+  const newEmployee = { ...employee
+  };
+  
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+  delete employee[key];
+  return employee;
+}
+
